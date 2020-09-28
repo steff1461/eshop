1e9c724d8a3693c7ab2635f1b6e7ffd8ca9d6933
steff1461 <45358565+steff1461@users.noreply.github.com>
2020-09-28 10:00:28 +0200

    first commit

---
+++ src/test/java/be/technobel/eshop/EshopApplicationTests.java
@@ -0,0 +1,13 @@
+package be.technobel.eshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
